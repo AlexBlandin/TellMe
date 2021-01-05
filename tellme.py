71c5886d4ae40eedf2df033a643b0e765f7400d3
Alex Blandin <20842735+AlexBlandin@users.noreply.github.com>
2021-01-05 22:37:03 +0000

    Bot

--- tellme.py
+++ tellme.py
@@ -1,4 +1,21 @@
 import discord
 import logging
+from discord.ext import commands
 # discord.AudioSource
-# permissions should be 53540864 (send messages ... attach files, connect, speak, move members, use voice activity)
+# permissions 53540928: send messages ... attach files, add reactions, connect, speak, move members, use voice activity
+
+with open("../tellme-token.txt","r") as o: TOKEN=o.read().strip()
+bot = commands.Bot(">")
+bot.owner_id = 234272777446621185
+
+@commands.is_owner()
+@bot.command()
+async def ping(ctx):
+  await ctx.send("Running")
+
+@commands.is_owner()
+@bot.command()
+async def logout(ctx):
+  await ctx.bot.logout()
+
+bot.run(TOKEN)
